059a513e61711b4709358495688613f160641458
Varada Kalyan <155228435+kalyanvsm@users.noreply.github.com>
2026-07-24 15:49:10 +0530

    Add: 4sum - submission-2

---
+++ Data Structures & Algorithms/4sum/submission-2.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+
+        for(int a = 0; a < nums.size() - 3; a++){
+            if(a > 0 && nums[a] == nums[a - 1]) continue;
+            //int t1 = target - nums[a];
+
+            for(int b = a + 1; b < nums.size() - 2; b++){
+                if(b > a + 1 && nums[b] == nums[b - 1]) continue;
+                //int t2 = t1 - nums[b];
+
+                int c = b + 1;
+                int d = nums.size() - 1;
+
+                while(c < d){
+
+                    long long sum =
+                        (long long)nums[a] +
+                        nums[b] +
+                        nums[c] +
+                        nums[d];
+
+                    if(sum > target) d--;
+                    else if(sum < target) c++;
+                    else{
+                        ans.push_back({nums[a], nums[b], nums[c++], nums[d--]});
+                        while(c < d && nums[c] == nums[c - 1]) c++;
+                        while(c < d && nums[d] == nums[d + 1]) d--;
+                    }
+                }
+            }
+
+        }
+
+        return ans;
+    }
+};
